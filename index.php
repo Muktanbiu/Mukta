e961a59da547fd35df099b97599d4380c61ffeef
Mukta <developermukta19@gmail.com>
2023-08-23 10:00:42 +0600

    h1 and p

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <h1>mukta pagol</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, vero expedita cupiditate quibusdam quis eligendi facilis maxime reprehenderit quod iste beatae earum, officia, qui deleniti nulla esse. Ab, unde at?</p>
 </body>
 </html>
